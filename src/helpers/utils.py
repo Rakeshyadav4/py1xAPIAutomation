38a943006b3951f045ef4be2bb0a51b03c3c1e92
Rakeshyadav4 <Rakeshyadav.bangalore@gmail.com>
2024-01-29 16:31:05 +1100

    13 Dec 2022

--- src/helpers/utils.py
+++ src/helpers/utils.py
@@ -5,11 +5,17 @@ def common_headers_json():
     }
     return headers
 
+def common_headers_for_put_delete_patch():
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Basic YWRtaW46cGFzc3dvcmQxMjM=",
+    }
+    return headers
+
 
 def common_headers_xml():
     headers = {
         "Content-Type": "application/xml",
     }
     return headers
-
 # Read data from Excel, csv, json, YAML - Keep the fucntions in future
--- tests/integration_test/test_crud.py
+++ tests/integration_test/test_crud.py
@@ -2,21 +2,28 @@ import pytest
 
 from src.constants.api_constants import APIConstants
 from src.helpers.api_requests_wrapper import post_requests,put_requests
+from src.helpers.api_requests_wrapper import post_requests, put_requests
 from src.helpers.common_verification import verify_response_key_should_not_be_none, verify_http_status_code
 from src.helpers.payload_manager import payload_create_booking, payload_create_token
 from src.helpers.utils import common_headers_json
+from src.helpers.utils import common_headers_json, common_headers_for_put_delete_patch
 
 
 class TestCreateBooking(object):
 
     def test_create_token(self):
+    @pytest.fixture()
+    def create_token(self):
         response = post_requests(
             url=APIConstants.url_create_token(),
             headers=common_headers_json(),
             auth = None,
             payload= payload_create_token(), in_json=False
+            auth=None,
+            payload=payload_create_token(), in_json=False
         )
         verify_http_status_code(response,200)
+        verify_http_status_code(response, 200)
         token = response.json()["token"]
         print(token)
         verify_response_key_should_not_be_none(token)
@@ -24,28 +31,38 @@ class TestCreateBooking(object):
 
 
     def test_create_booking(self):
+    @pytest.fixture()
+    def create_booking(self):
         response = post_requests(url=APIConstants.url_create_booking(), auth=None, headers=common_headers_json(),
                                  payload=payload_create_booking(), in_json=False)
         print(response)
-        bookingid = response.json()["bookingid"]
-        print(bookingid)
+
+    def test_create_booking(self):
         verify_response_key_should_not_be_none(response.json()["bookingid"])
         verify_http_status_code(response, 200)
         return bookingid
     def test_update_booking(self): # Token and Booking ID from the Create Booking, Token
         token = "58d8c38d7019749"
-        put_url = APIConstants.url_create_booking() + "/1163"  # here we are hard coded the booking number.
+        put_url = APIConstants.url_create_booking() + "/2698"
         auth = ("admin","password123")
 
         response = put_requests(url=put_url, auth=auth, headers=common_headers_json(),
                                  payload=payload_create_booking(), in_json=False)
+    def test_update_booking(self, create_token,
+                            create_booking):  # Token/ Basic Auth and Booking ID from the Create Booking, Token
+        bookindId = create_booking
+        put_url = APIConstants.url_create_booking() + "/" + str(bookindId)
+
+        response = put_requests(url=put_url, headers=common_headers_for_put_delete_patch(), auth=None,
+                                payload=payload_create_booking(), in_json=False)
         print(response.json())
 
     def test_delete_booking(self):
         pass
+    def test_delete_booking(self, create_token, create_booking):  # Token and Booking ID from the Create Booking, Token
+        bookindId = create_booking
+        delete_url = APIConstants.url_create_booking() + "/" + str(bookindId)
 
-
-# In your test module or conftest.py
-import pytest
-
-pytest.mark.negative = pytest.mark.mark(name="negative")
+        response = put_requests(url=delete_url, headers=common_headers_for_put_delete_patch(), auth=None,
+                                payload=None, in_json=False)
+        print(response.json())

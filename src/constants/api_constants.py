1f5c9496d7366e1f4b905db419f2e38d13dff4db
Rakeshyadav4 <Rakeshyadav.bangalore@gmail.com>
2024-01-29 12:27:32 +1100

    11 Dec 2023

--- src/constants/api_constants.py
+++ src/constants/api_constants.py
@@ -1,29 +1,27 @@
-#add you constants here
-# we can use three ways to call URL like directly
-Base_URL = "https://restful-booking.herokuapp.com"
+# Add your constants here
 
- #  or with normal function
+BASE_URL = "https://restful-booker.herokuapp.com"
+
+
 def base_url():
-    return "https://restful-booking.herokuapp.com"
+    return "https://restful-booker.herokuapp.com"
+# BASE_URL = "https://restful-booker.herokuapp.com"
+#
+#
+# def base_url():
+#     return "https://restful-booker.herokuapp.com"
 
- #  or within the class also called as object oriented programming method
+
 class APIConstants(object):
-
     @staticmethod
-    def base_url(): # for staticmethod we don't need self
-        return "https://restful-booking.herokuapp.com"
-
+    def base_url():
+        return "https://restful-booker.herokuapp.com"
     @staticmethod
     def url_create_booking():
-        return "https://restful-booking.herokuapp.com/booking"
-
+        return "https://restful-booker.herokuapp.com/booking"
     @staticmethod
     def url_create_token():
-        return "https://restful-booking.herokuapp.com/auth"
-
-    #above we are using staticmethod and below no static because when we run TC1 or TC2 or TC3 above function will be same--
-    #where as below it changes as it depended on what booking id you get
-
-    def url_patch_put_delete_booking(self,booking_id):  # here self comma not dot because it is self and booking_id
-        return "https://restful-booking.herokuapp.com/booking/" + str(booking_id)
-
+        return "https://restful-booker.herokuapp.com/auth"
+    # Update, PUT, PATCH, DELETE -booking id
+    def url_patch_put_delete_booking(self, booking_id):
+        return "https://restful-booker.herokuapp.com/booking/" + str(self.booking_id)
